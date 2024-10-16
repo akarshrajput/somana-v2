bfecad372b61b545b97bf62a6a2f6510a287b73a
Akarsh Rajput <akarshrajput@Akarshs-MacBook-Air.local>
2024-10-17 00:16:09 +0530

    Commited

--- app/api/v1/blogs/slug/[slug]/route.js
+++ app/api/v1/blogs/slug/[slug]/route.js
@@ -20,11 +20,29 @@ export async function GET(request) {
       );
     }
 
+    const cookies = request.cookies.get("viewedBlog"); // Retrieve cookie if exists
+    let hasViewed = cookies && cookies.includes(slug);
+
+    // Handle views (using userId or IP)
     if (userId) {
+      // Check if the userId is already in the views array
       if (!blog.views.includes(userId)) {
         blog.views.push(userId);
         await blog.save();
       }
+    } else if (!hasViewed) {
+      const ip = getIpAddress(request); // Get user's IP address
+      if (ip && !blog.views.includes(ip)) {
+        blog.views.push(ip); // Store the IP address as a view
+        await blog.save();
+      }
+      // Set cookie to mark blog as viewed
+      const response = NextResponse.json(
+        { status: "success", message: "Blog found successfully", data: blog },
+        { status: 200 }
+      );
+      response.cookies.set("viewedBlog", slug, { maxAge: 24 * 60 * 60 }); // Cookie valid for 24 hours
+      return response;
     }
 
     // Handle like action (only if userId is present)
